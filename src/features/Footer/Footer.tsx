e73ab011453245091f1149f845a79d6a624b8a85
Daniel-C-Martins <danicoxa.martins@outlook.com>
2025-01-24 16:46:03 -0300

    beginnig the landing page's footer

---
+++ src/features/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer: React.FC = () => {
+   return (
+      <div className="footer-container">
+         <div className="footer-bar"> </div>
+         <div className="footer-logos"> </div>
+         <div className="footer-infos>"></div>
+         <div className="footer-qr-code"></div>
+      </div>
+   );
+}
+
+export default Footer;
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,12 +1,14 @@
 import Header from "@features/Header/Header";
 import React from "react";
 import ListingCourse from "@features/ListingCourse";
+import Footer from "@features/Footer";
 
 const LandingPage: React.FC = () => {
   return (
     <>
       <Header />
       <ListingCourse></ListingCourse>
+      <Footer></Footer>
     </>
   );
 };
