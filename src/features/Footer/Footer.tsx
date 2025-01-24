a728411dda424098b64d6d8789f049c1b1b05a94
Daniel-C-Martins <danicoxa.martins@outlook.com>
2025-01-24 17:46:28 -0300

    Adding linkTo to landing page's logos

--- src/features/Footer/Footer.tsx
+++ src/features/Footer/Footer.tsx
@@ -3,22 +3,20 @@ import "./Footer.scss";
 import FooterLogo from "@components/FooterLogo";
 
 const Footer: React.FC = () => {
-   return (
-      <>
-         <div className="footer-container">
-            <div className="footer-bar"> </div>
-            <div className="footer-components">
-               <div className="footer-logos">
-                  <FooterLogo>
-
-                  </FooterLogo>
-               </div>
-               <div className="footer-infos>"></div>
-               <div className="footer-qr-code"></div>
-            </div>
-         </div>
-      </>
-   );
-}
+  return (
+    <>
+      <div className="footer-container">
+        <div className="footer-bar"> </div>
+        <div className="footer-components">
+          <div className="footer-logos">
+            <FooterLogo></FooterLogo>
+          </div>
+          <div className="footer-infos>"></div>
+          <div className="footer-qr-code"></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Footer;
