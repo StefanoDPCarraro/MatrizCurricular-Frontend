375259b21f92557da4624a859c663f22ca0d6f7f
Daniel-C-Martins <danicoxa.martins@outlook.com>
2025-01-30 00:45:27 -0300

    Creating a logic for matriz

--- src/mocks/Requirements.ts
+++ src/mocks/Requirements.ts
@@ -3,30 +3,37 @@ import { Subject } from "@dtos/SubjectDTO";
 export const requirements: Subject[] = [
   {
     creditCode: "ABCDE123",
-    name: "Fundamentos de Programação"
+    name: "Fundamentos de Programação",
+    semester: 1
   },
   {
     creditCode: "EDCBA321",
-    name: "Matematica Discreta"
+    name: "Matematica Discreta",
+    semester: 4
   },
   {
     creditCode: "XYZ98765",
-    name: "Formação do Empreendedor"
+    name: "Formação do Empreendedor",
+    semester: 2
   },
   {
     creditCode: "PRG00145",
-    name: "Estruturas de Dados"
+    name: "Estruturas de Dados",
+    semester: 5
   },
   {
     creditCode: "BD10230",
-    name: "Banco de Dados"
+    name: "Banco de Dados",
+    semester: 3
   },
   {
     creditCode: "WEB54321",
-    name: "Empreendedorismo Social Inovação e Responsabilidade Social"
+    name: "Empreendedorismo Social Inovação e Responsabilidade Social",
+    semester: 7 
   },
   {
     creditCode: "IA90012",
-    name: "Inteligência Artificial"
+    name: "Inteligência Artificial",
+    semester: 1
   }
 ];
--- src/pages/Matriz/Matriz.tsx
+++ src/pages/Matriz/Matriz.tsx
@@ -5,18 +5,26 @@ import Semester from "@features/Semester/Semester";
 import { requirements } from "@mocks/Requirements";
 
 const Matriz: React.FC = () => {
+  const semesters = Array.from({ length: 8 }, (_, i) => i + 1); // Cria [1, 2, 3, ..., 8]
+
   return (
     <>
       <Header course="Engenharia de Software - 98AL"></Header>
       <div className="matriz-container">
-        <Semester subjects={requirements}></Semester>
-        <Semester subjects={requirements}></Semester>
-        <Semester subjects={requirements}></Semester>
-        <Semester subjects={requirements}></Semester>
-        <Semester subjects={requirements}></Semester>
-        <Semester subjects={requirements}></Semester>
-        <Semester subjects={requirements}></Semester>
-        <Semester subjects={requirements}></Semester>
+        <div className="matriz-semesters">
+          {semesters.map((sem) => {
+            const filteredSubjects = requirements.filter(
+              (sub) => sub.semester === sem
+            );
+
+            return (
+              <React.Fragment key={sem}>
+                <Semester subjects={filteredSubjects} semester={sem} />
+                <div className="matriz-divisor"></div>
+              </React.Fragment>
+            );
+          })}
+        </div>
       </div>
     </>
   );
